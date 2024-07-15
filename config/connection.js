3f83ca26500a02a6266669aa10bb6bf41ca04392
stevelomax1 <steven.lomax1@gmail.com>
2024-07-14 23:22:21 -0400

    updates and fixes

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/socialnetwork')
+mongoose.connect("mongodb://localhost:27017/socialnetwork");
 
 module.exports = mongoose.connection;
